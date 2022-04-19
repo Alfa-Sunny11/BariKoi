f657f614a73278a4d378a7acce38092a21c9a36c
Alfa Sunny <alfasunny93@gmail.com>
2022-04-20 00:17:59 +0600

    create fake dataset

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Student;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StudentFactory extends Factory
+{
+    /**
+     * @var string
+     */
+    protected $model = Student::class;    
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "name" => $this->faker->name(),
+            "gender" => $this->faker->randomElement([
+                "male",
+                "female",
+                "others"
+            ]),
+            "phoneNo" => $this->faker->phoneNumber(),
+           
+            //
+        ];
+    }
+}
