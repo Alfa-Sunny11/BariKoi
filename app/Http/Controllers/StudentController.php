22c8bf5bea8e94ef9c0f23b1b62d0e6bb1fb5f2d
Alfa Sunny <alfasunny93@gmail.com>
2022-04-20 01:48:27 +0600

    API created

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     //
+    public function list(){
+
+           $students = Student::all();
+   
+           return view('welcome')->with('student',$students);
+       }
 }
